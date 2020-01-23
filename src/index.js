0d513df8794874ea7575b34447ee416670c1a86c
Alexandru Coman <alexandru.coman@pitechplus.com>
2020-01-23 13:13:54 +0200

    CORS fix

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ require('./db/mongoose');
 const userRouter = require('./routes/User');
 const taskRouter = require('./routes/Task')
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+})
+
 app.use(express.json())
 // Routing
 
